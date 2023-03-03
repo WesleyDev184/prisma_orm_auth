f1a140d5049c59a449839e1e57d690c9c63599d5
Wesley Antonio <57929638+WesleyDev184@users.noreply.github.com>
2023-03-02 21:04:40 -0300

    Update animalRoutes.ts

--- src/routes/animalRoutes.ts
+++ src/routes/animalRoutes.ts
@@ -3,9 +3,10 @@ import { AnimalCreate, AnimalGetAll } from "../controllers/animalController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const animalRoutes = (app: Application) => {
+  app.route("/animal").get(AnimalGetAll);
+
   app.use(ensureAuthenticated);
   app.route("/animal/register").post(AnimalCreate);
-  app.route("/animal").get(AnimalGetAll);
 };
 
 export default animalRoutes;
